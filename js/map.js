446df14a38aa2a183232953310342f6d49d1a99e
sed8c8 <sed8c8@umsl.edu>
2022-11-18 18:34:24 -0600

    incom-plete user input

--- js/map.js
+++ js/map.js
@@ -8,21 +8,21 @@ if (screen.width < 560) {
 
 /**********************************************************************************************/
 
-// Validate text box area
+// Validate text box area (no javaScript added for the actual text inside the textbox)
     var userInput = document.getElementById("userText");
     var resultLength = document.getElementById("result");
-    var limit = 280;
+    var limit = 180;
     resultLength.textContent = 0 + "/" + limit;
 
     userInput.addEventListener("keyup", function () {
         var textLength = userInput.value.length;
         resultLength.textContent = textLength + "/" + limit;
-        // if user enters more than 280 characters, border color and limit color will be red
+        // if user enters more than 180 characters, border color and limit color will be red
         if (textLength > limit) {
             userInput.style.borderColor = "#ff2851";
             resultLength.style.color = "#ff2851";
         }
-        // If characters are less than 280, border and limit color are set to green
+        // If characters are less than and equal to 180, border and limit color are set to green
         else {
             userInput.style.borderColor = "#008000";
             resultLength.style.color = "#008000";
@@ -35,10 +35,6 @@ if (screen.width < 560) {
     const previewDisplayed = document.getElementById("imagePreview");
     const previewImage = previewDisplayed.querySelector(".image-preview__image");
     const previewDefaultText = previewDisplayed.querySelector(".image-preview__default-text");
-    const imageDescription = document.getElementById("imageDescription");
-    var imageResultLength = document.getElementById("imageResult");
-    var imageLimit = 25;
-    imageResultLength.textContent = 0 + "/" + imageLimit;
 
     userImage.addEventListener("change", function () {
         const file = this.files[0];
@@ -58,25 +54,29 @@ if (screen.width < 560) {
         else {
             previewDefaultText.style.display = null;
             previewImage.style.display = null;
-                document.getElementById("imageDescription").disabled = false;
-
-                imageDescription.addEventListener("keyup", function () {
-                    var imageTextLength = imageDescription.value.length;
-                    imageResultLength.textContent = imageTextLength + "/" + imageLimit;
-                    // if user enters more than 280 characters, border color and limit color will be red
-                    if (imageTextLength > imageLimit) {
-                        imageDescription.style.borderColor = "#ff2851";
-                        imageResultLength.style.color = "#ff2851";
-                    }
-                    // If characters are less than 280, border and limit color are set to green
-                    else {
-                        imageDescription.style.borderColor = "#008000";
-                        imageDescription.style.color = "#008000";
-                    }
-                });
+                //document.getElementById("imageDescription").disabled = false;
 
         }
     });
 
 /*********************************************************************************************/
 
+// TextBox for image
+var imageDescription = document.getElementById("imageDescription");
+var imageResultLength = document.getElementById("imageResult");
+var imageLimit = 50;
+imageResultLength.textContent = 0 + "/" + imageLimit;
+imageDescription.addEventListener("keyup", function () {
+    var imageTextLength = imageDescription.value.length;
+    imageResultLength.textContent = imageTextLength + "/" + imageLimit;
+    // if user enters more than 280 characters, border color and limit color will be red
+    if (imageTextLength > imageLimit) {
+        imageDescription.style.borderColor = "#ff2851";
+        imageResultLength.style.color = "#ff2851";
+    }
+    // If characters are less than or equal to 50, border and limit color are set to green
+    else {
+        imageDescription.style.borderColor = "#008000";
+        imageResultLength.style.color = "#008000";
+    }
+});
